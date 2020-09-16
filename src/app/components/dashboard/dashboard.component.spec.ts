e27dad5ea193fac356708cfee6d9368bf2cf8878
Martin Campagnoli <mcampagnoli@canto.com>
2020-09-16 16:46:38 +0200

    Add unit tests

--- src/app/components/dashboard/dashboard.component.spec.ts
+++ src/app/components/dashboard/dashboard.component.spec.ts
@@ -10,7 +10,9 @@ const mockAuthService = {
   currentUser: currentUserSubject.asObservable()
 }
 const mockFeedService = {
-  getFeed: () => from([])
+  getFeed: () => from([]),
+  addLike: (param) => false,
+  saveComment: (post, test) => false
 };
 
 describe('DashboardComponent', () => {
@@ -36,4 +38,76 @@ describe('DashboardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should return false if object is not in given array', () => {
+    let obj = {uid: 1};
+    let list = [];
+    list.push(1);
+    list.push(2);
+    expect(component.containsObject(obj, list)).toBeFalsy();
+  });
+  it('should return true if object is in given array', () => {
+    let obj = {uid: 1};
+    let list = [];
+    list.push(1);
+    list.push(2);
+    list.push(obj);
+    expect(component.containsObject(obj, list)).toBeTruthy();
+  });
+  it('should toggle show values on post object', () => {
+    let initialValue = Math.random() < 0.5;
+    let obj = {show: initialValue};
+    component.toggle(obj);
+    expect(obj.show).toBe(!initialValue);
+  });
+  it('should call service method with given parameter', () => {
+    let obj = {show: true};
+    spyOn(mockFeedService, "addLike")
+    component.addLike(obj);
+
+    expect(mockFeedService.addLike).toHaveBeenCalled();
+    expect(mockFeedService.addLike).toHaveBeenCalledWith(obj);
+  });
+  it('should return true if user is able to like comment: logged user did not give like yet', () => {
+    component.currentUser = {name:"test", uid:"1"}
+    let post = {likesAuthors: ["2"]};
+    expect(component.canLike(post)).toBeTruthy();
+  });
+  it('should return false if user is not able to like comment: not logged user', () => {
+    component.currentUser = null;
+    let post = {likesAuthors: ["2"]};
+    expect(component.canLike(post)).toBeFalsy();
+  });
+  it('should return false if user is not able to like comment: user already gave like', () => {
+    component.currentUser = {name:"test", uid:"1"}
+    let post = {likesAuthors: ["1"]};
+    expect(component.canLike(post)).toBeFalsy();
+  });
+  it('should do nothing if comment is an empty string', () => {
+    let index = 1;
+    let post = {};
+    component.newComment[index] = '';
+    spyOn(mockFeedService, "saveComment");
+    component.saveComment(post, index);
+    expect(mockFeedService.saveComment).not.toHaveBeenCalled();
+  });
+  it('should call function to store comment if not empty string', () => {
+    let index = 1;
+    let post = {};
+    component.newComment[index] = 'test comment';
+    let value = component.newComment[index];
+    spyOn(mockFeedService, "saveComment");
+    component.saveComment(post, index);
+    expect(mockFeedService.saveComment).toHaveBeenCalled();
+    expect(mockFeedService.saveComment).toHaveBeenCalledWith(value, post);
+  });
+  it('should clear comment string after storing it', () => {
+    let index = 1;
+    let post = {};
+    component.newComment[index] = 'test comment';
+    
+    spyOn(mockFeedService, "saveComment");
+    component.saveComment(post, index);
+    expect(component.newComment[index]).toBe('');
+  });
+
 });
